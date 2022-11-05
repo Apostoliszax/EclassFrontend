36b5627e660c6aca1a1dc8d5ca1fb90250e7bc28
apost <apost@DESKTOP-77GJSMF>
2022-11-05 23:38:34 +0200

    added navbar with routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-import { ReactDOM } from "react-dom";
-import { Register } from "./Register";
-import { Login } from "./Login";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Navigation/Navbar.js";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Courses } from "./components/Courses";
+import { Grades } from "./components/Grades";
+import { Home } from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand as={Link} to="/Home">
+                Eclass++
+              </Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/Courses">
+                  Courses
+                </Nav.Link>
+                <Nav.Link as={Link} to="/Grades">
+                  Features
+                </Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
+          <Routes>
+            <Route path="/Home" element={<Home />}></Route>
+            <Route path="/Courses" element={<Courses />}></Route>
+            <Route path="/Grades" element={<Grades />}></Route>
+          </Routes>
+        </>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Grades.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Grades = () => {
+  return <div>Grades</div>;
+};
