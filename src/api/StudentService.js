ad5b1896b1e5b3fca968531ca48bb395a782092f
apost <apost@DESKTOP-77GJSMF>
2022-11-06 19:10:00 +0200

    Added Course and Professor  service, corrected student

--- src/api/StudentService.js
+++ src/api/StudentService.js
@@ -4,7 +4,7 @@ const getAll = () => {
   return http.get("/students");
 };
 const get = (id) => {
-  return http.get(`/students${id}`);
+  return http.get(`/students/${id}`);
 };
 const create = (data) => {
   return http.post("/students", data);
